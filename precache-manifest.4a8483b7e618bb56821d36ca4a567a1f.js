a9c231aec3f1cceb2090b67d13c64be2ccb4a682
LarryEmerson <larryemerson@163.com>
2019-03-13 10:36:33 +0800

    no message

---
+++ precache-manifest.4a8483b7e618bb56821d36ca4a567a1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "293907fad40b9fa58617d8e6c4dcfc49",
+    "url": "/static/media/global.293907fa.less"
+  },
+  {
+    "revision": "078ba7a3ea3b6469b4ec87b46ec495ae",
+    "url": "/static/media/antd.078ba7a3.less"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b732bba3c97fb628e771",
+    "url": "/static/js/main.f2d0ee1c.chunk.js"
+  },
+  {
+    "revision": "8721e694e5b685012aab",
+    "url": "/static/js/2.ea8a45ea.chunk.js"
+  },
+  {
+    "revision": "b732bba3c97fb628e771",
+    "url": "/static/css/main.889e56e9.chunk.css"
+  },
+  {
+    "revision": "8721e694e5b685012aab",
+    "url": "/static/css/2.b7840324.chunk.css"
+  },
+  {
+    "revision": "bd2878bcce85d773f385dd21fac8c700",
+    "url": "/index.html"
+  }
+];
